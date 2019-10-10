8436e458141f4f6f71fb848bc79cb36582560ed9
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-10 19:59:24 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,10 @@
 def using_until
   levitation_force = 6
-  #your code here
-    
+  
+until levitation_force >= 10
+  puts "#{the leaviation_force} is less than 10 ."
+  leaviation_force -=
 end
+
+    
 
